d66b20941c5726be23e064cc58c1afca7d2376d5
TinyPNG <TinyPNG@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-02-12 15:42:55 +0000

    First attempt at setting up a development workflow using Docker.
    git-svn-id: https://plugins.svn.wordpress.org/tiny-compress-images/trunk@1088204 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ tinypng-image-compression.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Plugin Name: TinyPNG image compression
+ * Plugin URI: http://tinypng.com
+ * Description: Compressing PNG and JPG images using the TinyPNG web service.
+ * Version: 0.1.0
+ * Author: TinyPNG
+ * Author URI: http://voormedia.com/about
+ * License: GPL2
+ */
+
+?>
